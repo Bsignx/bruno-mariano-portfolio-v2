ab42689f2cd6d8462884e977b83f80252e78a773
Bruno Mariano <bsignx@gmail.com>
2022-05-29 17:05:21 -0300

    feat: create h3 heading

--- app/components/typography.tsx
+++ app/components/typography.tsx
@@ -6,3 +6,9 @@ export const Paragraph = styled("p", {
   fontWeight: "$regular",
   lineHeight: "1.3125rem",
 });
+
+export const Heading3 = styled("h3", {
+  fontSize: "$4",
+  fontWeight: "$semibold",
+  color: "$hiContrast",
+});
