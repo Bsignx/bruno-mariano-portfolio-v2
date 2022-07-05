0138a6b6f06bb632f4f34e6daf6a9f788069c327
Bruno Mariano <bsignx@gmail.com>
2022-07-05 08:11:28 -0300

    feat: setup pwa

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
-import { getCssText } from "./styles/stitches.config";
-import { ClientStyleContext } from "./styles";
+import * as React from 'react';
+import { RemixBrowser } from '@remix-run/react';
+import { hydrate } from 'react-dom';
+import { getCssText } from './styles/stitches.config';
+import { ClientStyleContext } from './styles';
 
 interface ClientCacheProviderProps {
   children: React.ReactNode;
@@ -14,6 +14,14 @@ function ClientCacheProvider({ children }: ClientCacheProviderProps) {
   const reset = React.useCallback(() => {
     setSheet(getCssText());
   }, []);
+
+  // if the browser supports SW (all modern browsers do it)
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      // we will register it after the page complete the load
+      navigator.serviceWorker.register('/sw.js');
+    });
+  }
 
   return (
     <ClientStyleContext.Provider value={{ reset, sheet }}>
