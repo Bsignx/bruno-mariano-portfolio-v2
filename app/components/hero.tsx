6fd4e0acfa926750be798425e0ee74d31890fd43
Bruno Mariano <bsignx@gmail.com>
2022-07-05 08:11:44 -0300

    refactor: update hero image width

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,108 +1,104 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 import {
   AiFillGithub as GithubIcon,
   AiFillLinkedin as LinkedinIcon,
-} from "react-icons/ai";
+} from 'react-icons/ai';
 
-import { animateOnScrollVariants, useAnimateOnScroll } from "~/helpers";
-import { styled, theme } from "~/styles";
-import type { Portfolio } from "~/types";
-import { Cta } from "./cta";
+import { animateOnScrollVariants, useAnimateOnScroll } from '~/helpers';
+import { styled, theme } from '~/styles';
+import type { Portfolio } from '~/types';
+import { Cta } from './cta';
 
 const Container = styled(motion.section, {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  marginTop: "$20",
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '$20',
   zIndex: 1,
 
-  "@tabletUp": {
-    flexDirection: "row",
-    alignItems: "center",
+  '@tabletUp': {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
-const InfoContainer = styled("div", {});
+const InfoContainer = styled('div', {});
 
-const Title = styled("h1", {
-  color: "$hiContrast",
-  fontSize: "$7",
-  fontWeight: "$semibold",
-  lineHeight: "100%",
+const Title = styled('h1', {
+  color: '$hiContrast',
+  fontSize: '$7',
+  fontWeight: '$semibold',
+  lineHeight: '100%',
 
   span: {
-    background: "$highlight",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
-    backgroundClip: "text",
-    textFillColor: "transparent",
-    fontSize: "4rem",
+    background: '$highlight',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundClip: 'text',
+    textFillColor: 'transparent',
+    fontSize: '4rem',
   },
 
-  "@tabletUp": {
-    fontSize: "$8",
+  '@tabletUp': {
+    fontSize: '$8',
   },
 
-  "@desktopUp": {
-    fontSize: "$10",
+  '@desktopUp': {
+    fontSize: '$10',
 
     span: {
-      fontSize: "4.5rem",
+      fontSize: '4.5rem',
     },
   },
 });
 
-const Subtitle = styled("h2", {
-  color: "$hiContrast",
-  fontSize: "$5",
-  fontWeight: "$medium",
-  marginTop: "$4",
+const Subtitle = styled('h2', {
+  color: '$hiContrast',
+  fontSize: '$5',
+  fontWeight: '$medium',
+  marginTop: '$4',
 
-  "@tabletUp": {
-    fontSize: "$5",
-    marginTop: "$2",
+  '@tabletUp': {
+    fontSize: '$5',
+    marginTop: '$2',
   },
 
-  "@desktopUp": {
-    fontSize: "$6",
+  '@desktopUp': {
+    fontSize: '$6',
   },
 });
 
-const Description = styled("p", {
-  color: "$hiContrast",
-  fontSize: "$3",
-  fontWeight: "$regular",
-  marginTop: "$4",
-  marginBottom: "$6",
+const Description = styled('p', {
+  color: '$hiContrast',
+  fontSize: '$3',
+  fontWeight: '$regular',
+  marginTop: '$4',
+  marginBottom: '$6',
 });
 
-const CallToActionContainer = styled("div", {
-  display: "flex",
-  alignItems: "center",
+const CallToActionContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
   svg: {
-    marginLeft: "$4",
+    marginLeft: '$4',
   },
 });
 
-const SocialLink = styled("a", {
-  "&:hover": {
-    filter: "opacity(0.8)",
+const SocialLink = styled('a', {
+  '&:hover': {
+    filter: 'opacity(0.8)',
   },
 });
 
-const HeroImage = styled("img", {
-  marginTop: "$8",
-  display: "none",
-  marginLeft: "$6",
-  alignSelf: "flex-start",
+const HeroImage = styled('img', {
+  marginTop: '$8',
+  display: 'none',
+  marginLeft: '$6',
+  alignSelf: 'flex-start',
 
-  "@tabletUp": {
-    display: "block",
-    width: "25rem",
-  },
-
-  "@desktopUp": {
-    width: "28.125rem",
+  '@tabletUp': {
+    display: 'block',
+    width: '24rem',
   },
 });
 
