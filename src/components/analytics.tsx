c6f05395c4ca0e11044276396178537a6b2d2e27
Bruno Mariano <bsignx@gmail.com>
2022-07-21 08:27:41 -0300

    fix(analytics): add id to script next component

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -4,10 +4,12 @@ import { GA_TRACKING_ID } from '../helpers';
 export const Analytics = () => (
   <>
     <Script
+      id="gtm-script"
       strategy="afterInteractive"
       src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
     />
     <Script
+      id="gtm-script-2"
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
